69a84502f225dd5436d95736ce9cca6f5a0cc84a
王煜 <964934202@qq.com>
2019-08-31 23:12:10 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import './style/index.less' // 初始化样式
 import ElementUI from 'element-ui' // 引入
 import 'element-ui/lib/theme-chalk/index.css' // element样式
+import axios from 'axios' // 引入axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 省略基础地址
+Vue.prototype.$axios = axios //  将axios绑定在vue的原型上
+
 Vue.use(ElementUI) // 使用elementui
 
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,31 +1,31 @@
 <template>
-<!-- el-card是包裹el-form和el-form-item和el-inout -->
-<!-- el-form表单的容器 -->
+  <!-- el-card是包裹el-form和el-form-item和el-inout -->
+  <!-- el-form表单的容器 -->
   <div class="login">
-       <el-card class="box-card">
-            <!-- 顶部图片 -->
-            <img src="../../assets/img/logo_index.png" alt="黑马头条号">
-            <!-- 表单 -->
-            <!-- 数据校验需要先给el-form绑定数据对象 -->
-            <!-- rules绑定验证规则 -->
-            <el-form :model="loginForm" :rules="rules" style="margin-top:20px">
-                <!-- 添加prop属性,绑定需要校验的字段名 -->
-                <el-form-item prop="mobile">
-                    <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
-                </el-form-item>
-                <el-form-item prop="code">
-                    <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
-                    <el-button v-model="loginForm.check" style="float:right">发送验证码</el-button>
-                </el-form-item>
-                <el-form-item prop="check">
-                    <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-                </el-form-item>
-                <el-form-item>
-                    <!-- type="primary"element里的按钮组件 -->
-                    <el-button style="width:100%;" type="primary">登录</el-button>
-                </el-form-item>
-            </el-form>
-       </el-card>
+    <el-card class="box-card">
+      <!-- 顶部图片 -->
+      <img src="../../assets/img/logo_index.png" alt="黑马头条号" />
+      <!-- 表单 -->
+      <!-- 数据校验需要先给el-form绑定数据对象 -->
+      <!-- rules绑定验证规则 -->
+      <el-form :model="loginForm" :rules="rules" style="margin-top:20px">
+        <!-- 添加prop属性,绑定需要校验的字段名 -->
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="check">
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <!-- type="primary"element里的按钮组件 -->
+          <el-button style="width:100%;" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
@@ -34,6 +34,17 @@ export default {
   // 表单校验,先将数据和视图双向绑定,页面数据变化data里的属性可以立刻取到值
   // 定义表单内的数据(初始为空)
   data () {
+    // 里面有三个参数rule(验证规则), value(要验证的内容), callback(回调函数)
+    let validator = function (rule, value, callBack) {
+      // 如果勾选了(结果为true)则通过,执行回调函数
+      if (value === true) {
+        callBack() // 什么都不写为通过
+      } else {
+        // 为false,抛出错误信息
+        callBack(new Error('请勾选'))
+      }
+    }
+
     return {
       // 定义loginForm可以通过loginForm拿取到整个数据
       // 需要将loginForm数据对象绑定给el-form
@@ -45,16 +56,23 @@ export default {
       // 定义验证规则
       rules: {
         mobile: [
-          // 验证表单是否填写内容,required为true表示必须有内容,message表示没有输入内容时的提示信息
+          // 验证表单是否填写内容,required为true表示必须有内容,message表示没有输入内容时的提示信息,required只校验null '' undefined
           { required: true, message: '请输入手机号' },
           // 验证手机号格式是否正确,pattern正则表达式
           { pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确' }
         ],
         // 验证码不能为空
-        code: [{ required: true, message: '请输入验证码' },
+        code: [
+          { required: true, message: '请输入验证码' },
           // 验证码为六位
-          { pattern: /^\d{6}$/, message: '验证码格式不正确' }],
-        check: []
+          { pattern: /^\d{6}$/, message: '验证码格式不正确' }
+        ],
+        //   验证受否勾选,使用自定义校验函数validator
+        check: [
+          {
+            validator
+          }
+        ]
       }
     }
   }
@@ -64,25 +82,24 @@ export default {
 <style lang='less' scoped>
 // lang 为指定css语言
 // scoped 为只针对当前组件样式求起作用,要不然会全局生效
-    .login {
-        background-image: url('../../assets/img/login_bg.jpg');
-        /* vh单位为相对于可视区域的高度的百分比 */
-        height: 100vh;
-        // cover 将背景图片设置为覆盖整个可视区域
-        background-size: cover;
-        // flex布局,x\y轴水平居中
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .box-card {
-            width: 400px;
-            height: 330px;
-            img {
-                display: block;
-                width: 200px;
-                margin: 10px auto;
-            }
-        }
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  /* vh单位为相对于可视区域的高度的百分比 */
+  height: 100vh;
+  // cover 将背景图片设置为覆盖整个可视区域
+  background-size: cover;
+  // flex布局,x\y轴水平居中
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box-card {
+    width: 400px;
+    height: 330px;
+    img {
+      display: block;
+      width: 200px;
+      margin: 10px auto;
     }
-
+  }
+}
 </style>
